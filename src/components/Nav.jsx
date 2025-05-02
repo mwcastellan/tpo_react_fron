21cfd372620b81032ad24b941ec4d25a642c8af5
mcastellan1 <101480046+mcastellan1@users.noreply.github.com>
2025-05-01 22:45:52 -0300

    mwcastellan

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Carrusel from "../assets/img/carrusel1.png";
+import Carrito from "../assets/img/cart-shopping-solid.svg"
 
 const Nav = () => {
   return (
     <>
       <nav class="nav">
-        <ul>
+        <ul class="menu"> 
           <img src={Carrusel} width="90" height="70" />
           <li>
             <a href="index.html">Inicio</a>
@@ -23,6 +24,7 @@ const Nav = () => {
           <li>
             <a href="formulario.html">Contacto</a>
           </li>
+          <img src={Carrito} width="30" height="30" />
         </ul>
       </nav>
     </>
